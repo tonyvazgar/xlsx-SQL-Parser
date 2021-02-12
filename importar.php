b56d7e60e64f1422ce5f671f393d546db5095aef
Tony <user@Tony-Vazgars-iMac.local>
2021-02-12 12:17:07 -0600

    Backend casi listo

--- importar.php
+++ importar.php
@@ -1,23 +1,26 @@
 <?php
-
+    session_start(); 
 ?>
 <!DOCTYPE html>
 <html>
     <head>
-        <title>Importar producción</title>
+        <title>Importar producción</title>  
     </head>
     <body>
-        <form action="#" method="POST" enctype="multipart/form-data">
-            <input type="file" name="excel" id="excel" required>
-            <input type="submit" name="submit">
-        </form>
         <?php
-            if(isset($_FILES['excel']['name'])){
-                echo "Hola prro";
-                echo realpath($_FILES['excel']['name']);
-
+            if (isset($_SESSION['message']) && $_SESSION['message'])
+            {
+                printf('<b>%s</b>', $_SESSION['message']);
+                unset($_SESSION['message']);
             }
         ?>
-
+        <form method="POST" action="upload.php" enctype="multipart/form-data">
+            <div>
+                <span>Upload a File:</span>
+                <input type="file" name="uploadedFile" />
+            </div>
+        
+            <input type="submit" name="uploadBtn" value="Upload" />
+        </form>
     </body>
 </html>
---
+++ upload.php
@@ -0,0 +1,53 @@
+<?php
+session_start();
+require 'vendor/autoload.php';
+require 'parser.php';
+use PhpOffice\PhpSpreadsheet\Spreadsheet;
+use PhpOffice\PhpSpreadsheet\Writer\Xlsx;
+$message = '';
+if (isset($_POST['uploadBtn']) && $_POST['uploadBtn'] == 'Upload') {
+    if (isset($_FILES['uploadedFile']) && $_FILES['uploadedFile']['error'] === UPLOAD_ERR_OK) {
+        $fileTmpPath   = $_FILES['uploadedFile']['tmp_name'];
+        $fileName      = $_FILES['uploadedFile']['name'];
+        $fileSize      = $_FILES['uploadedFile']['size'];
+        $fileType      = $_FILES['uploadedFile']['type'];
+        $fileNameCmps  = explode(".", $fileName);
+        $fileExtension = strtolower(end($fileNameCmps));
+
+        // $newFileName   = md5(time() . $fileName) . '.' . $fileExtension;
+        $newFileName   = $fileName;
+
+        $allowedfileExtensions = array('xlsx');
+
+        if (in_array($fileExtension, $allowedfileExtensions)) {
+            // directory in which the uploaded file will be moved
+            $uploadFileDir = './uploaded_files/';
+            $dest_path     = $uploadFileDir . $newFileName;
+
+            if (move_uploaded_file($fileTmpPath, $dest_path)) {
+                $message = 'File is successfully uploaded. <br>' . 'Filename: ' . $newFileName. '<br>' . 'Path: ' . $dest_path;
+                //------------------------------------------------------------------------------------------------------------------
+                $obj = \PhpOffice\PhpSpreadsheet\IOFactory::load($dest_path);
+                $worksheet = $obj->getSheet(0);
+
+                echo '<table>' . "\n";
+                rollosTurno($worksheet, "2", 1);
+                rollosTurno($worksheet, "2", 2);
+                rollosTurno($worksheet, "2", 3);
+                echo '</table>' . PHP_EOL;
+                //------------------------------------------------------------------------------------------------------------------
+            } else {
+                $message = 'There was some error moving the file to upload directory. Please make sure the upload directory is writable by web server.';
+            }
+        } else {
+            $message = 'Upload failed. Allowed file types: ' . implode(',', $allowedfileExtensions);
+        }
+    } else {
+        $message = 'Upload failed. Allowed file types: ' . implode(',', $allowedfileExtensions);
+    }
+} else {
+    $message = 'Hubo un error al cargar el archivo, Please check the following error.<br>';
+    $message = 'Error: ' . $_FILES['uploadedFile']['error'];
+}
+$_SESSION['message'] = $message;
+// header('Location: importar.php');
